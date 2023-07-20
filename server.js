9bc06b6b5632a2c93c6bed495bfb5e7d877a43e7
798ankita <ankitasharmawins@gmail.com>
2023-07-20 11:24:08 +0530

    used route index file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 require('./src/config/index');
-const router = require("./src/router/userRoutes");
+const router = require("./src/router/index");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
