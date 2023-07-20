edd680d0ec432d012ad125a1e42b3c225feffd86
798ankita <ankitasharmawins@gmail.com>
2023-07-20 11:27:40 +0530

    change in routes

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -1,24 +1,20 @@
-const express = require("express");
-const router = express.Router();
 const userCtrl = require("../controller/userController");
-const userMiddleware = require("../middleware/userMiddleware");
 
 //route getting all existing users
-router.get("/users", userCtrl.getUsers);
+exports.routeAllUsers = userCtrl.getUsers;
 
 //route to create user
-router.post("/register", userMiddleware.data, userCtrl.postUsers);
+exports.routeRegisterUsers = userCtrl.postUsers;
 
 //route to get one user
-router.get("/user/:id", userCtrl.getUser);
+exports.routeOneUser= userCtrl.getUser;
 
 //route to delete a user
-router.delete("/user/:id",userMiddleware.verifyToken, userCtrl.deleteUser);
+exports.routeDeleteUser = userCtrl.deleteUser;
 
 //route to login a user
-router.patch("/login",userMiddleware.login,userCtrl.loginUser);
+exports.routeLoginUser = userCtrl.loginUser;
 
 //route to update a user
-router.patch("/user/:id",userMiddleware.verifyToken,userCtrl.updatedUser);
+exports.routeUpdateUser = userCtrl.updatedUser;
 
-module.exports = router;
