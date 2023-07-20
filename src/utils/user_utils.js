06b07be339d4b1e51095006c258a6703b24ba0d5
798ankita <ankitasharmawins@gmail.com>
2023-07-20 16:49:50 +0530

    updated code

--- src/utils/user_utils.js
+++ src/utils/user_utils.js
@@ -37,35 +37,36 @@ const userUtils = (user) => {
 };
 
 /*@Params:user
-  @Description:function to validate user data during registration
+  @Description:function to validate user data while updating
  */
   const updateUser = (user) => {
     const schema = Joi.object({
-      name: Joi.string().min(3).max(40),
+      name: Joi.string().min(3).max(40).optional(),
   
       username: Joi.string().alphanum()
        .min(3)
-      .max(30),
+      .max(30)
+      .optional(),
   
-      password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9@]{3,30}$")),
+      password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9@]{3,30}$")).optional(),
   
-      gender: Joi.string().max(10),
+      gender: Joi.string().max(10).optional(),
   
-      contact: Joi.number().integer().min(10),
+      contact: Joi.number().integer().min(10).optional(),
   
-      age: Joi.number().integer().min(3),
+      age: Joi.number().integer().min(3).optional(),
   
-      address: Joi.string().max(100),
+      address: Joi.string().max(100).optional(),
   
-      email: Joi.string() .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+      email: Joi.string() .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).optional(),
   
-      role: Joi.string().min(3).max(40),
+      role: Joi.string().min(3).max(40).optional(),
   
-      blood_group: Joi.string().max(15),
+      blood_group: Joi.string().max(15).optional(),
   
-      state: Joi.string().max(15),
+      state: Joi.string().max(15).optional(),
   
-      city: Joi.string().max(15),
+      city: Joi.string().max(15).optional(),
     })
   
     return schema.validate(user);
@@ -74,7 +75,14 @@ const userUtils = (user) => {
 
 //token validation
 const jwtLogin = (data) =>{
+  // console.log(data);
   return jwt.sign({data},process.env.JWT_SECRET_KEY, {expiresIn:"600s"});
 };
+
+//function to send responses
+
+// const sendResponse = () => {
+
+// }
 
 module.exports = {userUtils,updateUser,jwtLogin};
