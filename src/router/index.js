5511f566a5b6e14acc3379ec75f2f583a527aa06
798ankita <ankitasharmawins@gmail.com>
2023-07-20 11:27:10 +0530

    used userRoutes

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const userMiddleware = require("../middleware/userMiddleware");
+const userRoutes = require('./userRoutes');
+
+//route getting all existing users
+router.get("/users",userRoutes.routeAllUsers);
+
+//route to create user
+router.post("/register",userMiddleware.data,userRoutes.routeRegisterUsers);
+
+//route to get one user
+router.get("/user/:id",userRoutes.routeOneUser);
+
+//route to delete a user
+router.delete("/delete/:id",userMiddleware.verifyToken,userRoutes.routeDeleteUser);
+
+//route to login a user
+router.patch("/login",userMiddleware.login,userRoutes.routeLoginUser);
+
+//route to update a user
+router.patch("/updateUser/:id",userMiddleware.verifyToken,userMiddleware.updatedUser,userRoutes.routeUpdateUser);
+
+module.exports = router;
