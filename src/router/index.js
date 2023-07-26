728739ebc090feeec04b68e6f89448eb273f8bff
798ankita <ankitasharmawins@gmail.com>
2023-07-26 14:57:38 +0530

    added blood bank routes and logout route for user

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userMiddleware = require("../middleware/userMiddleware");
 const userRoutes = require('./userRoutes');
 
+
+//*********************************User Routes*********************************/
 //route getting all existing users
 router.get("/users",userRoutes.routeAllUsers);
 
@@ -10,7 +12,7 @@ router.get("/users",userRoutes.routeAllUsers);
 router.post("/register",userMiddleware.data,userRoutes.routeRegisterUsers);
 
 //route to get one user
-router.get("/user/:id",userRoutes.routeOneUser);
+router.get("/user",userMiddleware.verifyToken,userRoutes.routeOneUser);
 
 //route to delete a user
 router.delete("/delete",userMiddleware.verifyToken,userRoutes.routeDeleteUser);
@@ -18,10 +20,20 @@ router.delete("/delete",userMiddleware.verifyToken,userRoutes.routeDeleteUser);
 //route to login a user
 router.patch("/login",userMiddleware.login,userRoutes.routeLoginUser);
 
-// //route to logout a user
-// router.put("/logout/:id",userMiddleware.verifyToken,userRoutes.routelogoutUser);
+//route to logout a user
+router.put("/logout",userMiddleware.verifyToken,userRoutes.routelogoutUser);
 
 //route to update a user
 router.put("/updateUser",userMiddleware.verifyToken,userMiddleware.updatedUser,userRoutes.routeUpdateUser);
+
+
+/*********************************blood_bank_Routes******************************/
+
+//route to create blood_bank
+router.post("/register/blood_bank",userMiddleware.data,userRoutes.routeRegisterUsers);
+
+//route to login a blood_bank
+router.patch("/login/blood_bank",userMiddleware.login,userRoutes.routeLoginUser);
+
 
 module.exports = router;
