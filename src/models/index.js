2365af224d0c2d90655fb4bf75dd9ca843c51e3d
798ankita <ankitasharmawins@gmail.com>
2023-07-27 18:46:36 +0530

    updated model index file using migrations

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,49 @@
-const {Sequelize,DataTypes} = require('sequelize');
-const sequelize = require("../config/index");
+'use strict';
+const  {Sequelize, DataTypes} = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+// const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
   db.Sequelize = Sequelize;
   db.sequelize = sequelize;
-  db.user = require("./user_model")(sequelize,DataTypes);
-  // db.superUser = require("./superUser")(sequelize,DataTypes);
+  db.user = require("./user")(sequelize,DataTypes);
+  db.request = require("./actionModel")(sequelize,DataTypes);
   db.sequelize.sync();
   console.log("All models were synchronized successfully.");
-  module.exports = db;
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+
+module.exports = db;
