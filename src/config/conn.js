a05e266f1df469bc6524b3ac0f169940bafaed8f
798ankita <ankitasharmawins@gmail.com>
2023-07-27 18:50:29 +0530

    chaged file name

---
+++ src/config/conn.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PWD,
+  {
+    host: process.env.DB_HOST,
+    logging: false,
+    dialect: "mysql",
+  }
+);
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} 
+catch (error) {
+console.error("Unable to connect to the database:", error);
+}
+module.exports = sequelize;
