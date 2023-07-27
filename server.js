40f6c12b0dbd65e025e3ebccd60539b0cf378e8f
798ankita <ankitasharmawins@gmail.com>
2023-07-27 18:51:29 +0530

    update in  code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
-require('./src/config/index');
+// require('./src/config/index');
 const router = require("./src/router/index");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
