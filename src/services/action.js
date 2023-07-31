31df87ec1b50b70557b48fca5a22477a30dfe703
798ankita <ankitasharmawins@gmail.com>
2023-07-31 17:27:25 +0530

    updated action services to cancel requests

---
+++ src/services/action.js
@@ -0,0 +1,41 @@
+const db = require("../models/index");
+const action = db.action;
+
+ /*@Params:id
+  @Description:function to cancel the request.
+ */
+  exports.cancelRequestForBld =  async (id) =>
+  {
+    try {
+      const request = await action.update({status:"cancelled"},
+      {where:{ id : id }});
+      return request ;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+ /* @Params:id,data
+   @Description:This function find request by id.
+*/
+exports.findId = async (id) => {
+    try {
+      const requestId = await action.findOne({ where: {id:id } });
+      return requestId;
+    } 
+    catch (err) {
+    throw err;
+    }
+  };
+
+/*@Params:usersData
+  @Description:function to get all requests data
+*/
+exports.allRequests = async (data) => {
+  try {
+    const users = await action.findAll({});
+    return users;
+  } catch (err) {
+    throw err;
+  }
+};
