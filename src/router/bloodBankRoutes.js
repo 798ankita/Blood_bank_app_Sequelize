a77006c6d9e0c77cd80b88c4384768c4c3c9f930
798ankita <ankitasharmawins@gmail.com>
2023-07-31 17:31:49 +0530

    route for bldbank ,create, getall and see bloodReq

---
+++ src/router/bloodBankRoutes.js
@@ -0,0 +1,12 @@
+const bloodBankCtrl = require("../controller/bloodBankController");
+const actionCtrl = require("../controller/actionController");
+
+/***************************************bloodBank details*********************************/
+//route to add blood bank details.
+exports.createBloodBank = bloodBankCtrl.bldBankDetails;
+
+//route to see all blood banks list.
+exports.allBloodBankDetail = bloodBankCtrl.allBloodBanks;
+
+//route to see all blood requests created by user
+exports.allRequestsForBlood = actionCtrl.getAllBloodRequests;
