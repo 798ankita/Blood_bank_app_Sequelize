c0ac3e8d56a16f724af81edbf6053535aa94a94b
798ankita <ankitasharmawins@gmail.com>
2023-08-01 18:24:05 +0530

    added blood inventory table

---
+++ src/services/bloodInventory.js
@@ -0,0 +1,44 @@
+const db = require("../models/index");
+const bloodInventory = db.bloodInventory;
+
+/* @Params:data
+   @Description:This function adding details to blood Inventory table.
+*/
+exports.addInventory = async (data) => {
+    try {
+      const detail = await bloodInventory.create(data);
+      return detail;
+    } 
+    catch (err) {
+    throw err;
+    }
+  };
+
+  /*@Params:id
+  @Description:This function find existing user's id
+*/
+exports.findId = async(id) => {
+    try {
+      const data = await bloodInventory.findOne({ where: {id:id} });
+      return data;
+    } 
+  
+    catch (err) {
+    throw err;
+    }
+  };
+  
+/*@Params:id
+  @Description:This function to check blood availability
+*/
+exports.findBlood = async(id) => {
+    try {
+      const data = await bloodInventory.findAll({ });
+      return data;
+    } 
+    catch (err) {
+    throw err;
+    }
+  };
+  
+  
