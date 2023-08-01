90317b4f7f280a7a39c254e5ffd1b93b0a2b3687
798ankita <ankitasharmawins@gmail.com>
2023-08-01 14:51:25 +0530

    added payment details model

---
+++ src/models/paymentDetails.js
@@ -0,0 +1,47 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class paymentDetail extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+    models.bloodBank.hasMany(paymentDetail);
+    paymentDetail.belongsTo(models.bloodBank);
+
+    models.User.hasMany(paymentDetail);
+    paymentDetail.belongsTo(models.User);
+
+    models.userAction.hasOne(paymentDetail);
+    paymentDetail.belongsTo(models.userAction);
+
+    }
+  }
+
+  paymentDetail.init(
+    {
+      total_amount: {
+        type: DataTypes.STRING
+      },
+      payment_mode: {
+        type: DataTypes.STRING,
+      },
+      created_by: {
+        type: DataTypes.STRING,
+      },
+      updated_by: {
+        type: DataTypes.STRING,
+      }
+    },
+    {
+      sequelize,
+      modelName: "paymentDetail",
+      tableName: "paymentDetail",
+      paranoid: true,
+      timestamps: true,
+    }
+  );
+  return paymentDetail;
+};
