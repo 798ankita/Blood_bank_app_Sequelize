2a436891c8821f813c5f09c722a82220a2c3816c
798ankita <ankitasharmawins@gmail.com>
2023-08-02 19:11:51 +0530

    updated bloodbank controller

--- src/controller/bloodBankController.js
+++ src/controller/bloodBankController.js
@@ -1,6 +1,7 @@
 const userService = require("../services/user_service");
 const bloodBankService = require("../services/bloodBank");
 const inventoryService = require("../services/bloodInventory");
+const bloodPriceService = require("../services/bloodPrice");
 const { success, error  } = require("../utils/user_utils");
 const data = require("../middleware/userMiddleware");
 
@@ -29,7 +30,7 @@ return error(res,"Permission denied","do not have permission to add blood bank o
     
  } catch (err) {
    console.log(err);
-   return error(res,"error","Internal servser error!",500);
+   return error(res,"error","Internal server error!",500);
  }
     
 };
@@ -51,13 +52,12 @@ exports.allBloodBanks = async (req, res) => {
 exports.bloodInventory = async(req,res) => {
    try {
   const userId = req.data;
-//   console.log(userId)
   const userToken = await userService.userId(userId);
   const bloodBankId = await bloodBankService.findId(userToken.id);
   const allData = await inventoryService.findId(bloodBankId.id);
-  if(bloodBankId.status == "active" && allData == null || allData < 1)
+  if(userToken.role == "blood_bank" && bloodBankId.status == "active" && allData == null || allData < 1)
    {
-      const details = await inventoryService.addInventory({
+      const details = await bloodPriceService.addBloodPrice({
       AB_positive:req.body.AB_positive,
       A_positive:req.body.A_positive,
       B_positive:req.body.B_positive,
@@ -82,3 +82,85 @@ exports.bloodInventory = async(req,res) => {
    return error(res,"error","internal server error",500);
    }
 };
+
+//controller to add blood price.
+exports.bloodPrice = async(req,res) => {
+   try {
+  const userId = req.data;
+  const userToken = await userService.userId(userId);
+  const bloodBankId = await bloodBankService.findId(userToken.id);
+  const inventoryData = await inventoryService.findId(bloodBankId.id);
+  const priceData = await bloodPriceService.findId(bloodBankId.id);
+  if(userToken.role == "blood_bank" && bloodBankId.status == "active" && inventoryData != null && priceData == null)
+   {
+      const details = await bloodPriceService.addBloodPrice({
+      AB_positive:req.body.AB_positive,
+      A_positive:req.body.A_positive,
+      B_positive:req.body.B_positive,
+      O_positive:req.body.O_positive,
+      AB_negative:req.body.AB_negative,
+      A_negative:req.body.A_negative,
+      B_negative:req.body.B_negative,
+      O_negative:req.body.O_negative,
+      created_by:userToken.username,
+      updated_by:userToken.username,
+      bloodBankId:bloodBankId.id
+     });
+     return success(res,details, "blood prices added successfully", 200);
+   }
+   else{
+      return error(res,"permission denied","already added prices",208);
+   }
+     
+   }
+   catch(err){
+   console.log(err);
+   return error(res,"error","Permission denied",403);
+   }
+};
+
+
+//update inventory details.
+exports.updatedInventory = async (req, res) => {
+   try {
+   const userToken = await userService.userId(req.data);
+   const bloodBank = await bloodBankService.findId(userToken.id);
+   if(bloodBank.status =="active" && userToken.role == "blood_bank")
+   {
+      const data = await inventoryService.updateInventory(userToken.id, req.body);
+      return success(res, data, "inventory updated successfully", 200);
+   }
+   else{
+      return error(res,error, "process denied", 403)
+   }
+   
+      
+   } catch (err) {
+      console.log(err);
+      return error(res,error, "process denied", 403)
+   }
+   
+ };
+
+ //update blood price per unit details.
+exports.updateBloodPrice = async (req, res) => {
+   try {
+   const userToken = await userService.userId(req.data);
+   const bloodBank = await bloodBankService.findId(userToken.id);
+   const bloodBankId = await bloodPriceService.findId(bloodBank.id);
+   if(bloodBank.status =="active" && userToken.role == "blood_bank")
+   {
+      const data = await bloodPriceService.updatePriceDetails(userToken.id, req.body);
+      return success(res, data, "blood price updated successfully", 200);
+   }
+   else{
+      return error(res,error, "permission denied", 403)
+   }
+   
+      
+   } catch (err) {
+      console.log(err);
+      return error(res,error, "permission denied", 403)
+   }
+   
+ };
