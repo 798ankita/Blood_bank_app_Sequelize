04012f0e313a38e0fe738543ffeeff7cb19b5786
798ankita <ankitasharmawins@gmail.com>
2023-08-07 09:53:35 +0530

    bill generate

--- src/services/paymentDetail.js
+++ src/services/paymentDetail.js
@@ -14,3 +14,31 @@ exports.paymentBill = async (data) => {
     }
   };
   
+/* @Params:data
+   @Description:This function adding data  in bill after sending blood request.
+*/
+  exports.updateAmount = async(id,data) => {
+  try {
+    const payment = await paymentDetails.update({status:"approved",total_amount:data},
+     { where:{
+        id:id
+      }});
+    return payment;
+  } 
+  catch (err) {
+    console.log(err);
+  }
+};
+
+/* @Params:id
+   @Description:This function find request id in payment table
+*/
+exports.findReqId = async (id) => {
+  try {
+    const requestId = await paymentDetails.findOne({ where: {userActionId:id} });
+    return requestId;
+  } 
+  catch (err) {
+  throw err;
+  }
+};
