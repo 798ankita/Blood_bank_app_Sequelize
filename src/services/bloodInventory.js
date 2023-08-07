701970c5a19ab99497f81d3927ef7eb42cdd31d4
798ankita <ankitasharmawins@gmail.com>
2023-08-07 12:42:47 +0530

    updated services

--- src/services/bloodInventory.js
+++ src/services/bloodInventory.js
@@ -5,78 +5,85 @@ const bloodInventory = db.bloodInventory;
    @Description:This function adding details to blood Inventory table.
 */
 exports.addInventory = async (data) => {
-    try {
-      const detail = await bloodInventory.create(data);
-      return detail;
-    } 
-    catch (err) {
+  try {
+    const detail = await bloodInventory.create(data);
+    return detail;
+  } catch (err) {
     throw err;
-    }
-  };
+  }
+};
 
-  /*@Params:bloodBankId
+/*@Params:bloodBankId
   @Description:This function find existing user's id
 */
-exports.findId = async(bloodBankId) => {
-    try {
-      const data = await bloodInventory.findOne({ where: {bloodBankId:bloodBankId} });
-      return data;
-    } 
-  
-    catch (err) {
+exports.findId = async (bloodBankId) => {
+  try {
+    const data = await bloodInventory.findOne({
+      where: { bloodBankId: bloodBankId },
+    });
+    return data;
+  } catch (err) {
     throw err;
-    }
-  };
-  
+  }
+};
+
 /*@Params:id
   @Description:This function to check blood availability
 */
-exports.findInventory= async(id) => {
-    try {
-      const data = await bloodInventory.findOne({where:{id:id}});
-      return data;
-    } 
-    catch (err) {
-  throw err;
-    }
-  };
-  
-  /*@Params:id,data
+exports.findInventory = async (id) => {
+  try {
+    const data = await bloodInventory.findOne({ where: { id: id } });
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+/*@Params:id,data
   @Description:function to update blood Inventory units.
   */
-exports.updateInventory = async (id,data) => {
+exports.updateInventory = async (id, data) => {
   try {
-    const detail = await bloodInventory.update(data,{
-      where:{
-        id:id 
-      }    
-  });
+    const detail = await bloodInventory.update(data, {
+      where: {
+        id: id,
+      },
+    });
     return detail;
-  } 
-   catch (err) {
-   throw err;
-   }
+  } catch (err) {
+    throw err;
+  }
 };
-  
-  /*@Params:id, blood, unit
+
+/*@Params:bloodId, data
   @Description:function to update blood Inventory units automaticaly.
   */
-  exports.updateAutoInventory = async(bloodId, data)=>{
+exports.updateAutoInventory = async (bloodId, data) => {
+  try {
+    const change = await bloodInventory.update(data, {
+      where: {
+        bloodBankId: bloodId,
+      },
+    });
+    return change;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+/*@Params:bloodId, data
+  @Description:function to update blood Inventory units automaticaly when donation complete.
+  */
+  exports.inventoryUpdateDonation = async (bloodId,data) => {
     try {
-      const change = await bloodInventory.update(
-        data,
-        {
-          where:{
-            bloodBankId:bloodId
-          }
-        }
-      )
+      const change = await bloodInventory.update({data,status:"donation Complete"},{
+        where: {
+          bloodBankId: bloodId,
+        },
+      });
       return change;
     } catch (err) {
       console.log(err);
     }
-   
   };
-
-
-
+  
