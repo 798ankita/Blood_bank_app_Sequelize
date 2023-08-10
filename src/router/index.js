d63a479143189423bf0b43b2686e3376f16ab62b
798ankita <ankitasharmawins@gmail.com>
2023-08-10 22:07:21 +0530

    updated routes

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,13 @@ const bloodBank = require('./bloodBankRoutes');
 router.get('/users', userRoutes.routeAllUsers);
 
 // route to create user
-router.post('/register', userRoutes.routeRegisterUsers);
+router.post('/register',userRoutes.routeRegisterUsers);
 
 // route to get one user
-router.get('/user', userMiddleware.verifyToken, userRoutes.routeOneUser);
+router.get('/user', userMiddleware.verifyToken,userMiddleware.roleCheckUser,userRoutes.routeOneUser);
 
 // route to delete a user
-router.delete('/delete', userMiddleware.verifyToken, userRoutes.routeDeleteUser);
+router.delete('/delete', userMiddleware.verifyToken,userMiddleware.roleCheckUser,userRoutes.routeDeleteUser);
 
 // route to login a user
 router.patch('/login', userMiddleware.login, userRoutes.routeLoginUser);
@@ -26,22 +26,22 @@ router.patch('/login', userMiddleware.login, userRoutes.routeLoginUser);
 router.put('/logout', userMiddleware.verifyToken, userRoutes.routelogoutUser);
 
 // route to update a user
-router.put('/updateUser', userMiddleware.verifyToken, userMiddleware.updatedUser, userRoutes.routeUpdateUser);
+router.put('/updateUser', userMiddleware.verifyToken, userRoutes.routeUpdateUser);
 
 // route to see list of all blood banks.
 router.get('/allBloodBanksList', bloodBank.allBloodBankDetail);
 
 // route to create requests for blood by  patient user
-router.post('/user/patient/sendBloodRequest', userMiddleware.verifyToken, userRoutes.SendBloodRequestPatient);
+router.post('/user/patient/sendBloodRequest', userMiddleware.verifyToken,userMiddleware.roleCheckUser,userRoutes.SendBloodRequestPatient);
 
 // route to send request to donate blood by donor user
-router.post('/user/donor/sendRequest', userMiddleware.verifyToken, userRoutes.SendBloodRequestdonor);
+router.post('/user/donor/sendRequest', userMiddleware.verifyToken,userMiddleware.roleCheckUser,userRoutes.SendBloodRequestdonor);
 
 // route to cancel the request for blood.
-router.patch('/user/cancelBloodRequest', userMiddleware.verifyToken, userRoutes.routeCancelBloodRequest);
+router.patch('/user/cancelBloodRequest', userMiddleware.verifyToken,userMiddleware.roleCheckUser,userRoutes.routeCancelBloodRequest);
 
 // route to complete payment by user
-router.patch('/user/completePayment', userMiddleware.verifyToken, userRoutes.completePayment);
+router.patch('/user/completePayment', userMiddleware.verifyToken,userMiddleware.roleCheckUser,userRoutes.completePayment);
 
 /** *******************************blood_bank_Routes***************************** */
 
@@ -52,38 +52,38 @@ router.post('/register/blood_bank',userRoutes.routeRegisterUsers);
 router.patch('/login/blood_bank', userMiddleware.login, userRoutes.routeLoginUser);
 
 // route to add blood bank details.
-router.post('/addBloodBankDetails', userMiddleware.verifyToken, bloodBank.createBloodBank);
+router.post('/addBloodBankDetails', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank,bloodBank.createBloodBank);
 
 // route to get all created requests for blood by users.
-router.get('/BloodBank/allBloodRequests', userMiddleware.verifyToken, bloodBank.allRequestsForBlood);
+router.get('/BloodBank/allBloodRequests', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank, bloodBank.allRequestsForBlood);
 
 // route to accept blood requests created by user.
-router.patch('/bloodBank/acceptBloodRequests', userMiddleware.verifyToken, bloodBank.acceptBloodRequests);
+router.patch('/bloodBank/acceptBloodRequests', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank,bloodBank.acceptBloodRequests);
 
 // route to add blood inventory details.
-router.post('/bloodBank/addInventoryDetail', userMiddleware.verifyToken, bloodBank.addBloodInventory);
+router.post('/bloodBank/addInventoryDetail', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank,bloodBank.addBloodInventory);
 
 // route to update blood inventory details.
-router.patch('/bloodBank/updateInventoryDetail', userMiddleware.verifyToken, bloodBank.updateBloodInventory);
+router.patch('/bloodBank/updateInventoryDetail', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank,bloodBank.updateBloodInventory);
 
 // route to add blood price details.
-router.post('/bloodBank/addBloodPrice', userMiddleware.verifyToken, bloodBank.addBloodPrice);
+router.post('/bloodBank/addBloodPrice', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank, bloodBank.addBloodPrice);
 
 // route to update blood price details.
-router.patch('/bloodBank/updateBloodPrice', userMiddleware.verifyToken, bloodBank.updateBloodPrice);
+router.patch('/bloodBank/updateBloodPrice', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank,bloodBank.updateBloodPrice);
 
 // route to increment in blood unit while collecting blood
-router.patch('/bloodBank/bloodCollected', userMiddleware.verifyToken, bloodBank.IncrementBlood);
+router.patch('/bloodBank/bloodCollected', userMiddleware.verifyToken,userMiddleware.roleCheckBloodBank,bloodBank.IncrementBlood);
 
-/** *******************************SuperUser_Routes ****************************** */
+/*********************************SuperUser_Routes ****************************** */
 
 // route to login a super_user
 router.patch('/login/superUser', userMiddleware.login, userRoutes.routeLoginUser);
 
-router.get('/superUser/RequestsPending', userMiddleware.verifyToken, userRoutes.BloodBankRequest);
+router.get('/superUser/RequestsPending', userMiddleware.verifyToken,userMiddleware.roleCheckSuperUser,userRoutes.BloodBankRequest);
 
-router.put('/superUser/AcceptRequest', userMiddleware.verifyToken, userMiddleware.updatedUser, userRoutes.AcceptRequest);
+router.put('/superUser/AcceptRequest', userMiddleware.verifyToken,userMiddleware.roleCheckSuperUser,userRoutes.AcceptRequest);
 
-router.delete('/superUser/declineRequests', userMiddleware.verifyToken, userRoutes.declineRequests);
+router.delete('/superUser/declineRequests', userMiddleware.verifyToken,userMiddleware.roleCheckSuperUser,userRoutes.declineRequests);
 
 module.exports = router;
