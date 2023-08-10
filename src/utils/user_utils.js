cdbb4c98cf27a334df664ce41417eea88783b681
798ankita <ankitasharmawins@gmail.com>
2023-08-10 22:06:30 +0530

    minor change

--- src/utils/user_utils.js
+++ src/utils/user_utils.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 const getId = require('../services/user_service');
+const { success, error } = require('../utils/user_utils');
 // generate token
 exports.jwtLogin = async (data) => {
   const userId = await getId.checkUsername(data.username);
-  if (userId != null && userId.role == 'user') {
+  if (userId) {
     return jwt.sign(
       { id: userId.id, username: userId.username },
       process.env.JWT_SECRET_KEY,
