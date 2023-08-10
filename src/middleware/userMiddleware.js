6677934ba812dd3494ccfd2b48b4f57abf9bf8f6
798ankita <ankitasharmawins@gmail.com>
2023-08-10 16:51:16 +0530

    minor changes

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -1,25 +1,6 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable consistent-return */
-/* eslint-disable linebreak-style */
 const jwt = require('jsonwebtoken');
-const userMiddleware = require('../utils/user_utils');
 const { error } = require('../utils/user_utils');
-
-/* @Params:(req, res, next)
-  @Request:req.body
-  @Response:res.status(400)
-  @Description:Middleware for user registration
-*/
-
-exports.data = (req, res, next) => {
-  const user = req.body;
-  const response = userMiddleware.userUtils(user);
-  if (response.error) {
-    error(res, response.error.details[0].message, 'error occured while creating user', 400);
-  } else {
-    next();
-  }
-};
+const userMiddleware = require('../utils/user_utils');
 
 /* @params:req,res,next,
   @Request:req.body
@@ -45,7 +26,6 @@ exports.updatedUser = (req, res, next) => {
   @Description:login middleware with jwt token
 */
 exports.login = async (req, res, next) => {
-  // eslint-disable-next-line no-shadow
   const data = req.body;
   const token = await userMiddleware.jwtLogin(data);
   req.token = token;
